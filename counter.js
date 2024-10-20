56c274d3a231ba0c9fcab4244081158d1469d025
Muskan Chauhan <chauhanmuskam2006@gmail.com>
2024-10-20 16:35:36 +0530

    write the actual function that is export into the main.js file

--- counter.js
+++ counter.js
@@ -1,9 +1,22 @@
-export function setupCounter(element) {
-  let counter = 0
-  const setCounter = (count) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
+export function displayUsers(users) {
+  const userList = document.querySelector('.part2');
+  userList.innerHTML = ''; // Clear previous content
+
+  if (users.length === 0) {
+      userList.innerHTML = '<p>No users found above 30 years old.</p>';
+      return;
   }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
+
+  users.forEach(user => {
+      const userCard = `
+          <div class="user-card">
+              <h3>${user.firstName} ${user.lastName}</h3>
+              <p>Age: ${user.age}</p>
+              <p>Email: ${user.email}</p>
+              <p>Address: ${user.address.address}, ${user.address.city}, ${user.address.state}, ${user.address.country}</p>
+              <p>Birth Date: ${user.birthDate}</p>
+          </div>
+      `;
+      userList.innerHTML += userCard;
+  });
 }
