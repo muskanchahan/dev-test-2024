0f3e04fd384f611709e2d12eba0d93389bb7d3e7
Muskan Chauhan <chauhanmuskam2006@gmail.com>
2024-10-20 22:33:21 +0530

    Write the Javascript in from of the async and await

--- main.js
+++ main.js
@@ -8,16 +8,15 @@ document.addEventListener('DOMContentLoaded', function () {
     const fetchButton = document.querySelector('.fetchButton');
     fetchButton.addEventListener('click', fetchUsers);
 
-    function fetchUsers() {
-        fetch('https://dummyjson.com/users')
-            .then(response => response.json())
-            .then(data => {
-                // Filter users whose age is above 30
-                const users = data.users.filter(user => user.age > 30);
-                displayUsers(users); // Call the imported function
-            })
-            .catch(error => {
-                console.error('Error fetching users:', error);
-            });
+    async function fetchUsers() {
+        try {
+            const response = await fetch('https://dummyjson.com/users');
+            const data = await response.json();
+            // Filter users whose age is above 30
+            const users = data.users.filter(user => user.age > 30);
+            displayUsers(users); // Call the imported function
+        } catch (error) {
+            console.error('Error fetching users:', error);
+        }
     }
 });
