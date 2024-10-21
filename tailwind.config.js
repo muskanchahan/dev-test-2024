41357e97a6f5b8b821e44b2977e8897195ba2755
Muskan Chauhan <chauhanmuskam2006@gmail.com>
2024-10-21 17:27:00 +0530

    update the the tailwaind.config.js for color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,16 @@
 // tailwind.config.js
 export default {
-  content: ["*./.html"],
+  content: ["./*.html"],
   theme: {
       extend: {
           colors: {
-              primary: "#FC204D",
+              primary: "#FC204D", // Custom primary color
               slate: {
-                  800: '#1E293B',
-                  950: '#1E293B', 
+                  800: '#1E293B', // Slate gray
+                  950: '#000000', // Background color for cards and gutters
               },
           },
       },
   },
 };
+
