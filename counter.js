0df6ce2c71a03060a1b5172b7a508bc2bb9c4190
Muskan Chauhan <chauhanmuskam2006@gmail.com>
2024-10-21 17:23:57 +0530

    export the function called displayUser into the main.js

--- counter.js
+++ counter.js
@@ -1,7 +1,7 @@
-
+ 
 export function displayUsers(users) {
-    const userList = document.getElementById('userList'); // Get the user list element
-    userList.innerHTML = ''; // Clear previous content
+    const userList = document.getElementById('userList');  
+    userList.innerHTML = '';  
 
     if (users.length === 0) {
         userList.innerHTML = '<p class="text-white">No users found above 30 years old.</p>';
@@ -10,8 +10,8 @@ export function displayUsers(users) {
 
     users.forEach(user => {
         const userCard = `
-            <div class="user-card bg-black rounded text-white min-w-[200px] max-w-[300px] w-auto flex flex-col p-4 m-5 break-words">
-                <h2>${user.firstName} ${user.lastName}</h2>
+            <div class="user-card bg-black rounded-lg text-white w-64 h-64 p-4 overflow-hidden">
+                <h3 class="text-lg font-bold">${user.firstName} ${user.lastName}</h3>
                 <p>Age: ${user.age}</p>
                 <p>Email: ${user.email}</p>
                 <p>Address: ${user.address.address}, ${user.address.city}, ${user.address.state}, ${user.address.country}</p>
@@ -21,3 +21,4 @@ export function displayUsers(users) {
         userList.innerHTML += userCard; // Add the new user card to the user list
     });
 }
+ 
